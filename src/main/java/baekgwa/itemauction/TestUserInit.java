0d92f1b83c4dc987ba59c7771629f0918c61b903
KangBaekGwa <ksu9801@gmail.com>
2024-10-06 12:58:39 +0900

    Fix : 권한별 테스트 사용자 등록

--- src/main/java/baekgwa/itemauction/TestUserInit.java
+++ src/main/java/baekgwa/itemauction/TestUserInit.java
@@ -1,6 +1,7 @@
 package baekgwa.itemauction;
 
 import baekgwa.itemauction.domain.user.entity.User;
+import baekgwa.itemauction.domain.user.entity.UserRole;
 import baekgwa.itemauction.domain.user.repository.UserRepository;
 import baekgwa.itemauction.domain.userprofile.entity.UserProfile;
 import baekgwa.itemauction.domain.userprofile.repository.UserProfileRepository;
@@ -27,11 +28,30 @@ public class TestUserInit {
     @Transactional
     @EventListener(ApplicationReadyEvent.class)
     public void init() {
-        User newUser = User.createNewAdmin("test1", bCryptPasswordEncoder.encode("1234"));
-        User savedData = userRepository.save(newUser);
+        User newUser1 = User.createNewAdmin("admin1", bCryptPasswordEncoder.encode("1234"));
+        User savedData1 = userRepository.save(newUser1);
+        UserProfile newUserProfile1 = UserProfile.createNewUserProfile(savedData1, "테스트어드민A", "백과(어드민)",
+                "admintest@test.com", "01000000001");
+        userProfileRepository.save(newUserProfile1);
 
-        UserProfile newUserProfile = UserProfile.createNewUserProfile(savedData, "유저A", "백과",
-                "test@test.com", "01000000000");
-        userProfileRepository.save(newUserProfile);
+        User newUser2 = User.createNewAdmin("trader1", bCryptPasswordEncoder.encode("1234"));
+        newUser2.updateRole(UserRole.TRADER);
+        User savedData2 = userRepository.save(newUser2);
+        UserProfile newUserProfile2 = UserProfile.createNewUserProfile(savedData2, "테스트트레이더A", "백과(트레이더)",
+                "tradertest@test.com", "01000000002");
+        userProfileRepository.save(newUserProfile2);
+
+        User newUser3 = User.createNewAdmin("buyer1", bCryptPasswordEncoder.encode("1234"));
+        newUser3.updateRole(UserRole.BUYER);
+        User savedData3 = userRepository.save(newUser3);
+        UserProfile newUserProfile3 = UserProfile.createNewUserProfile(savedData3, "테스트구매자A", "백과(구매자)",
+                "buyertest@test.com", "01000000003");
+        userProfileRepository.save(newUserProfile3);
+
+        User newUser4 = User.createNewUser("none1", bCryptPasswordEncoder.encode("1234"));
+        User savedData4 = userRepository.save(newUser4);
+        UserProfile newUserProfile4 = UserProfile.createNewUserProfile(savedData4, "테스트미인증A", "백과(미인증)",
+                "nonetest@test.com", "01000000004");
+        userProfileRepository.save(newUserProfile4);
     }
 }
